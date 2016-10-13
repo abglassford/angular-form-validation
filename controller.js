3efe4892e6032b00d49db655ac07952cb68a430e
Alexander Glassford <a.b.glassford@gmail.com>
2016-10-13 16:39:10 -0600

    complete exercise

--- controller.js
+++ controller.js
@@ -2,4 +2,22 @@
   'use strict';
 
   const app = angular.module('validation', [])
+
+  app.controller('getInfo', getInfo)
+
+  function getInfo ($scope, $rootScope) {
+
+    $rootScope.users = []
+    $scope.retrieve = function (user){
+      $scope.taken = false
+      if ($rootScope.users.some(elem => elem.name === user.name)) {
+        $scope.taken = true
+      } else {
+        $rootScope.users.push(user)
+        $scope.user = null
+      }
+
+    }
+  }
+
 }());
