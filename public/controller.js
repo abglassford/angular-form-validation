1f6c20f4b29b185531a8b9a90d7c09919c3aa53c
Alexander Glassford <a.b.glassford@gmail.com>
2016-10-16 23:50:39 -0600

    fix form

--- public/controller.js
+++ public/controller.js
@@ -8,14 +8,16 @@
   function getInfo ($scope, $rootScope) {
 
     $rootScope.users = []
+
     $scope.retrieve = function (user){
-      console.log('!!', $rootScope.users.some(elem => elem.name === user.name));
       $scope.taken = false
       if ($rootScope.users.some(elem => elem.username === user.username)) {
         $scope.taken = true
       } else {
         $rootScope.users.push(user)
+        $scope.testForm.$setPristine()
         $scope.user = null
+
       }
 
     }
